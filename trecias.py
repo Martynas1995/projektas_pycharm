0f79032aecfbdf729d23416ce02e83991e572784
Martynas Katelynas <martynas.katelynas@gmail.com>
2023-10-26 13:53:02 +0300

    sukurtas trecias.py
    sukurtas failas su zodynu ir atprintinta zodyno raktai:vertes

---
+++ trecias.py
@@ -0,0 +1,10 @@
+d = {
+    "name": "Albert",
+    "surname": "Einstein",
+    "occupation": {
+        "role": "Professor",
+        "workplace": "Uni of Berlin",
+    },
+    "languages": ["German", "Latin", "Italian", "English"],
+}
+print(d.items())
